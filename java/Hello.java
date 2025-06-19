09aaaf1c1347e9157a2be874fab0df417fbdb843
qazwsx5381 <ryuyechan@lyuyechan-ui-noteubug.local>
2025-06-19 20:04:21 +0900

    first

---
+++ java/Hello.java
@@ -0,0 +1,5 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요, 정보처리기사!");
+    }
+}
---
+++ python/example.py
@@ -0,0 +1,5 @@
+# example.py
+def solution(a, b):
+    return a + b
+
+print(solution(3, 7))
